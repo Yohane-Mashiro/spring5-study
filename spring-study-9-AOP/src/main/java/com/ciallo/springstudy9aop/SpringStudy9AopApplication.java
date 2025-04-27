1367e47329d2c5ffb35aecd88d553d6dcbd28db2
Yohane-Mashiro <xuchengxi2006@outlook.com>
2025-04-27 20:16:46 +0800

    4/27

---
+++ spring-study-9-AOP/src/main/java/com/ciallo/springstudy9aop/SpringStudy9AopApplication.java
@@ -0,0 +1,13 @@
+package com.ciallo.springstudy9aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudy9AopApplication {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ spring-study-9-AOP/src/test/java/com/ciallo/springstudy9aop/SpringStudy9AopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciallo.springstudy9aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudy9AopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
